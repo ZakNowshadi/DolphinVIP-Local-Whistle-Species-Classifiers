639116304af9eda3f7099ec9ff196666686eed41
Emily Pondaven <83513279+emilypondaven@users.noreply.github.com>
2024-10-05 17:03:23 +0000

    made code work

--- PytorchImageClassifier/ComplicatedImageClassifier.py
+++ PytorchImageClassifier/ComplicatedImageClassifier.py
@@ -97,4 +97,20 @@ for epoch in range(2):  # loop over the dataset multiple times
 
 print('Finished Training')
 
+## CHECK ACCURACY ##
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in validationDataLoader:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
+
 
